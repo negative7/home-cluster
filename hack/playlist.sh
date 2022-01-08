b9221927b42458c27a09a0a116cb68c9cf7bb696
Sergiy Shevchenko <admin@negative7.com>
2022-01-08 17:21:07 +0000

    feat: gotask + updates

--- hack/playlist.sh
+++ hack/playlist.sh
@@ -9,10 +9,13 @@ while true; do
     kubecolor get po -n media; sleep 5; clear;
     figlet -f slant "home" | /usr/games/lolcat
     kubecolor get po -n home; sleep 5; clear;
-    figlet -f slant "tools" | /usr/games/lolcat
-    kubecolor get po -n tools; sleep 5; clear;
-    figlet -f slant "openfaas-fn" | /usr/games/lolcat
-    kubecolor get po -n openfaas-fn; sleep 5; clear;
     figlet -f slant "nodes" | /usr/games/lolcat
     kubecolor get nodes; sleep 5; clear;
+    figlet -f slant "media releases" | /usr/games/lolcat
+    kubecolor get helmreleases -n media; sleep 5; clear;
+    figlet -f slant "home" | /usr/games/lolcat
+    kubecolor get helmreleases -n home; sleep 5; clear;
+    figlet -f slant "kustomizations" | /usr/games/lolcat
+    kubecolor get kustomizations -A; sleep 5; clear;
 done
+– Arrival Letter
