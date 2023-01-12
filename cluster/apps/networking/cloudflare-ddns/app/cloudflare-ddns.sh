025a18a913e6e728349cd2e08cdb5733a02b94ba
Sergiy Shevchenko <admin@negative7.com>
2023-01-12 19:21:14 +0000

    networking

--- cluster/apps/networking/cloudflare-ddns/app/cloudflare-ddns.sh
+++ cluster/apps/networking/cloudflare-ddns/app/cloudflare-ddns.sh
@@ -9,7 +9,7 @@ zone_id=$(curl -s -X GET \
     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
     -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
-        | jq --raw-output ".result[0] | .id"
+    | jq --raw-output ".result[0] | .id"
 )
 record_ipv4=$(curl -s -X GET \
     "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${CLOUDFLARE_RECORD_NAME}&type=A" \
