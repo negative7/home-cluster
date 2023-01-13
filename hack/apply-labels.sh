f3ca676e7c2c8f5bfa1be423d1f8ee3335e4dcd7
Sergiy Shevchenko <admin@negative7.com>
2023-01-13 03:01:27 +0000

    plex

--- hack/apply-labels.sh
+++ hack/apply-labels.sh
@@ -6,3 +6,7 @@ for node in $NODES
 do
     kubectl label node $node k3s-upgrade=true
 done
+
+kubectl label node vinnytsia node-role.kubernetes.io/worker=true
+kubectl label node dnipro node-role.kubernetes.io/worker=true
+kubectl label node kyiv node-role.kubernetes.io/worker=true
