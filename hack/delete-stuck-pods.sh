47ff037fa5c5466770d6219acaedb9b1e3427d5b
Sergiy Shevchenko <admin@negative7.com>
2023-01-17 02:08:48 +0000

    misc

--- hack/delete-stuck-pods.sh
+++ hack/delete-stuck-pods.sh
@@ -6,61 +6,78 @@ function delete_pod () {
     kubectl delete pod -n $2 --grace-period=0 --force $pod
 }
 
-TERMINATING_PODS=$(kubectl get pods -n networking | grep Terminating | awk '{print $1}')
-NS="networking"
+NAMESPACES=(actions-runner-system cert-manager default home kube-system kyverno longhorn-system metallb-system monitoring networking system-upgrade)
 
-for pod in $TERMINATING_PODS
-do
-    delete_pod $pod $NS
+for NS in ${NAMESPACES[@]}; do
+    TERMINATING_PODS=$(kubectl get pods -n $NS | grep Terminating | awk '{print $1}')
+    for pod in $TERMINATING_PODS
+    do
+        delete_pod $pod $NS
+    done
+    TERMINATING_PODS=$(kubectl get pods -n $NS | grep Unknown | awk '{print $1}')
+    for pod in $TERMINATING_PODS
+    do
+        delete_pod $pod $NS
+    done
+    TERMINATING_PODS=$(kubectl get pods -n $NS | grep Evicted | awk '{print $1}')
+    for pod in $TERMINATING_PODS
+    do
+        delete_pod $pod $NS
+    done
 done
 
-TERMINATING_PODS3=$(kubectl get pods -n networking | grep Unknown | awk '{print $1}')
-
-for pod in $TERMINATING_PODS3
-do
-    delete_pod $pod $NS
-done
-
-TERMINATING_PODS2=$(kubectl get pods -n media | grep Terminating | awk '{print $1}')
-NS2="media"
-
-for pod2 in $TERMINATING_PODS2
-do
-    delete_pod $pod2 $NS2
-done
+# TERMINATING_PODS=$(kubectl get pods -n networking | grep Terminating | awk '{print $1}')
+# NS="networking"
 
 
 
-TERMINATING_PODS=$(kubectl get pods -n monitoring | grep Terminating | awk '{print $1}')
-NS="monitoring"
+# TERMINATING_PODS3=$(kubectl get pods -n networking | grep Unknown | awk '{print $1}')
 
-for pod in $TERMINATING_PODS
-do
-    delete_pod $pod $NS
-done
+# for pod in $TERMINATING_PODS3
+# do
+#     delete_pod $pod $NS
+# done
 
+# TERMINATING_PODS2=$(kubectl get pods -n media | grep Terminating | awk '{print $1}')
+# NS2="media"
 
-TERMINATING_PODS=$(kubectl get pods -n home | grep Terminating | awk '{print $1}')
-NS="home"
-
-for pod in $TERMINATING_PODS
-do
-    delete_pod $pod $NS
-done
+# for pod2 in $TERMINATING_PODS2
+# do
+#     delete_pod $pod2 $NS2
+# done
 
 
-TERMINATING_PODS=$(kubectl get pods -n kube-system | grep Evicted | awk '{print $1}')
-NS="kube-system"
 
-for pod in $TERMINATING_PODS
-do
-    delete_pod $pod $NS
-done
+# TERMINATING_PODS=$(kubectl get pods -n monitoring | grep Terminating | awk '{print $1}')
+# NS="monitoring"
 
-TERMINATING_PODS=$(kubectl get pods -n networking | grep Evicted | awk '{print $1}')
-NS="networking"
+# for pod in $TERMINATING_PODS
+# do
+#     delete_pod $pod $NS
+# done
 
-for pod in $TERMINATING_PODS
-do
-    delete_pod $pod $NS
-done
+
+# TERMINATING_PODS=$(kubectl get pods -n home | grep Terminating | awk '{print $1}')
+# NS="home"
+
+# for pod in $TERMINATING_PODS
+# do
+#     delete_pod $pod $NS
+# done
+
+
+# TERMINATING_PODS=$(kubectl get pods -n kube-system | grep Evicted | awk '{print $1}')
+# NS="kube-system"
+
+# for pod in $TERMINATING_PODS
+# do
+#     delete_pod $pod $NS
+# done
+
+# TERMINATING_PODS=$(kubectl get pods -n networking | grep Evicted | awk '{print $1}')
+# NS="networking"
+
+# for pod in $TERMINATING_PODS
+# do
+#     delete_pod $pod $NS
+# done
