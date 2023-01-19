f43253b2aed13ebab79ee5de22baf73ef3772cbb
Sergiy Shevchenko <admin@negative7.com>
2023-01-19 18:02:42 +0000

    bakup lables

--- hack/apply-labels.sh
+++ hack/apply-labels.sh
@@ -10,3 +10,10 @@ done
 kubectl label node vinnytsia node-role.kubernetes.io/worker=true
 kubectl label node dnipro node-role.kubernetes.io/worker=true
 kubectl label node kyiv node-role.kubernetes.io/worker=true
+
+
+kubectl get pvc -n default --no-headers | grep config | awk '{print $3}' | xargs -l bash -c 'kubectl -n longhorn-system label volume/$0 recurring-job.longhorn.io/backup-1=enabled'
+kubectl get pvc -n default --no-headers | grep config | awk '{print $3}' | xargs -l bash -c 'kubectl -n longhorn-system label volume/$0 recurring-job.longhorn.io/snapshot-1=enabled'
+
+kubectl get pvc -n home --no-headers | grep config | awk '{print $3}' | xargs -l bash -c 'kubectl -n longhorn-system label volume/$0 recurring-job.longhorn.io/backup-1=enabled'
+kubectl get pvc -n home --no-headers | grep config | awk '{print $3}' | xargs -l bash -c 'kubectl -n longhorn-system label volume/$0 recurring-job.longhorn.io/snapshot-1=enabled'
