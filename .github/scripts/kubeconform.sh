be6911c5ffb452a1a946925f1fd252c85cd8156b
Sergiy Shevchenko <admin@negative7.com>
2023-02-15 14:13:37 +0000

    misc github

--- .github/scripts/kubeconform.sh
+++ .github/scripts/kubeconform.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 KUBERNETES_DIR=$1
 SCHEMA_DIR=$2
-KUBE_VERSION="${3:-1.24.8}"
+KUBE_VERSION="${3:-1.26.0}"
 
 [[ -z "${KUBERNETES_DIR}" ]] && echo "Kubernetes location not specified" && exit 1
 [[ -z "${SCHEMA_DIR}" ]] && echo "Schema location not specified" && exit 1
@@ -26,31 +26,31 @@ kubeconform_args=(
 
 echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
-  do
+do
     kubeconform "${kubeconform_args[@]}" "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
+        exit 1
     fi
 done
 
 echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
+do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+    kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
+        exit 1
     fi
 done
 
 echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
+do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
     kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+    kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
+        exit 1
     fi
 done
