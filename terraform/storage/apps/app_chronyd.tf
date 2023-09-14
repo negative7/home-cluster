49ae41f65e311546a1fea3ebbc289485f4aefd39
Sergiy Shevchenko <admin@negative7.com>
2023-09-14 15:25:29 -0400

    nas

---
+++ terraform/storage/apps/app_chronyd.tf
@@ -0,0 +1,36 @@
+resource "kubernetes_daemon_set_v1" "chronyd" {
+  metadata {
+    name      = "chronyd"
+    namespace = "default"
+    labels = {
+      "app.arpa.home/name" = "chronyd"
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        "app.arpa.home/name" = "chronyd"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "app.arpa.home/name" = "chronyd"
+        }
+      }
+      spec {
+        container {
+          name              = "ntp"
+          image             = "docker.io/simonrupf/chronyd:0.4.3"
+          image_pull_policy = "IfNotPresent"
+        }
+      }
+      port {
+        name           = "ntp"
+        container_port = 123
+        host_port      = 123
+        protocol       = "udp"
+      }
+    }
+  }
+}
