4ff1ae8835bf95007ee13ff82bb85e1d5911c9b3
Sergiy Shevchenko <admin@negative7.com>
2023-09-14 20:37:01 -0400

    rebuild...

--- infrastructure/talos/cluster-0/deploy-integrations.sh
+++ infrastructure/talos/cluster-0/deploy-integrations.sh
@@ -9,9 +9,9 @@ rm cni/values.yaml
 rm -rf cni/charts
 
 rm -rf kubelet-csr-approver/charts
-envsubst < ../../../../kubernetes/cluster-0/apps/system-controllers/kubelet-csr-approver/app/values.yaml > kubelet-csr-approver/values.yaml
-if ! kubectl get ns system-controllers >/dev/null 2>&1; then
-  kubectl create ns system-controllers
+envsubst < ../../../../kubernetes/cluster-0/apps/kube-system/kubelet-csr-approver/app/values.yaml > kubelet-csr-approver/values.yaml
+if ! kubectl get ns kube-system >/dev/null 2>&1; then
+  kubectl create ns kube-system
 fi
 kustomize build --enable-helm kubelet-csr-approver | kubectl apply -f -
 rm kubelet-csr-approver/values.yaml
