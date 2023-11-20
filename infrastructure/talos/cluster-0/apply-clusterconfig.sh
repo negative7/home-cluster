3f8e250b42bb98f8b83af27c288fcbd525d01459
Sergiy Shevchenko <admin@negative7.com>
2023-11-20 12:14:17 -0500

    feat: update talos

--- infrastructure/talos/cluster-0/apply-clusterconfig.sh
+++ infrastructure/talos/cluster-0/apply-clusterconfig.sh
@@ -5,3 +5,8 @@ talosctl apply-config -n 10.2.0.21 -f ./clusterconfig/cluster-0-kharkiv.yaml
 talosctl apply-config -n 10.2.0.22 -f ./clusterconfig/cluster-0-vinnytsia.yaml 
 talosctl apply-config -n 10.2.0.23 -f ./clusterconfig/cluster-0-dnipro.yaml 
 talosctl apply-config -n 10.2.0.24 -f ./clusterconfig/cluster-0-kyiv.yaml
+
+talosctl upgrade --nodes 10.2.0.21 --image ghcr.io/siderolabs/installer:v1.5.5
+talosctl upgrade --nodes 10.2.0.22 --image ghcr.io/siderolabs/installer:v1.5.5
+talosctl upgrade --nodes 10.2.0.23 --image ghcr.io/siderolabs/installer:v1.5.5
+talosctl upgrade --nodes 10.2.0.24 --image ghcr.io/siderolabs/installer:v1.5.5
