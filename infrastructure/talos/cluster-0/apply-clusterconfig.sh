b8c5f5cc9f52d7283fb3971c607f20ce347d54da
Sergiy Shevchenko <admin@negative7.com>
2024-01-13 13:29:39 -0500

    talos upgrade

--- infrastructure/talos/cluster-0/apply-clusterconfig.sh
+++ infrastructure/talos/cluster-0/apply-clusterconfig.sh
@@ -6,8 +6,11 @@ talosctl apply-config -n 10.2.0.22 -f ./clusterconfig/cluster-0-vinnytsia.yaml
 talosctl apply-config -n 10.2.0.23 -f ./clusterconfig/cluster-0-dnipro.yaml 
 talosctl apply-config -n 10.2.0.24 -f ./clusterconfig/cluster-0-kyiv.yaml
 
-talosctl upgrade --nodes 10.2.0.21 --image factory.talos.dev/installer/22b1d04da881ef7c57edb0f24d1f3ba2c78a8e22cbe3fa9af4f42d487b2863f7:v1.5.5 
-talosctl upgrade --nodes 10.2.0.22 --image factory.talos.dev/installer/22b1d04da881ef7c57edb0f24d1f3ba2c78a8e22cbe3fa9af4f42d487b2863f7:v1.5.5 
-talosctl upgrade --nodes 10.2.0.23 --image factory.talos.dev/installer/22b1d04da881ef7c57edb0f24d1f3ba2c78a8e22cbe3fa9af4f42d487b2863f7:v1.5.5 
-talosctl upgrade --nodes 10.2.0.24 --image factory.talos.dev/installer/22b1d04da881ef7c57edb0f24d1f3ba2c78a8e22cbe3fa9af4f42d487b2863f7:v1.5.5 
+#upgrade cli to new version by  
+# rm /usr/local/bin/talosctl
+# curl -sL https://talos.dev/install | sh
 
+talosctl -n "10.2.0.21" upgrade
+talosctl -n "10.2.0.22" upgrade
+talosctl -n "10.2.0.23" upgrade
+talosctl -n "10.2.0.24" upgrade
