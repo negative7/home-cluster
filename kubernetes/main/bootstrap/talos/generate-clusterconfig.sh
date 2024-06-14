dfc903f7f0c1c75598921bca805506c9b9c59a89
Sergiy Shevchenko <admin@negative7.com>
2024-06-14 18:00:48 -0400

    feat(talos): refactor

--- kubernetes/main/bootstrap/talos/generate-clusterconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Talhelper
-
-# Generate a new config using talhelper
-talhelper genconfig --env-file talenv.sops.yaml --secret-file talsecret.sops.yaml --config-file talconfig.yaml
